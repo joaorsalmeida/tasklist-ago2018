32ff0ce5c34609bd1d1f59e1dd475f4f35d67cc6
Joao Almeida <joaorsalmeida@gmail.com>
2018-08-28 20:54:26 -0300

    Inserir e ver tarefas

---
+++ tasklist.rb
@@ -0,0 +1,33 @@
+def menu()
+  puts "\n\n"
+  puts "MENU"
+  puts "[1] Inserir uma tarefa"
+  puts "[2] Ver todas tarefas"
+  puts "[0] Sair"
+  print "Opção Escolhida: "
+  # problema de escopo de variaveis
+  opcao_esc = gets().to_i() #vou receber uma string com o que foi digitado
+  puts "\n\n"
+  return opcao_esc
+end
+
+puts "Bem-vindo ao TaskList"
+opcao = menu()
+tarefas = []
+
+while(opcao != 0) do
+  #tomar uma decisao: o que fazer a partir da opcao escolhida
+  if(opcao == 1)
+    print "Nome da Tarefa: "
+    tarefa = gets()
+    puts "Tarefa Cadastrada >>> " + tarefa
+    tarefas << tarefa
+  elsif(opcao == 2)
+    puts "Tarefas Cadastradas"
+    puts tarefas
+  end
+
+  opcao = menu()
+end
+
+puts "Adeus..."
